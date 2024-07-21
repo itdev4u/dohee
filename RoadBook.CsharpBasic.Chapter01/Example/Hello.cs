e0f663e89dc2d2c3f16f78a08cd2b4808564ff39
lunajin <luna7698@gmail.com>
2024-07-21 15:28:55 +0900

    초기 커밋.

---
+++ RoadBook.CsharpBasic.Chapter01/Example/Hello.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace RoadBook.CsharpBasic.Chapter01.Example
+{
+    public class Hello
+    {
+        public void Run()
+        {
+            Console.WriteLine("Hello.cs Run 입니다.");
+        }
+
+    }
+}
---
+++ RoadBook.CsharpBasic.Chapter01/Program.cs
@@ -0,0 +1,11 @@
+﻿namespace RoadBook.CsharpBasic.Chapter01
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Example.Hello hello = new Example.Hello();
+            hello.Run();
+        }
+    }
+}
